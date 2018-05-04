5b63d53fb551030b55e470507d1b10f6b956b28e
Madelline Butcher <madellinebutcher@gmail.com>
2018-05-03 22:32:54 -0600

    drawFunction added

--- app/footballController.js
+++ app/footballController.js
@@ -2,7 +2,23 @@ function FootballController(){
 //PRIVATE
 var footballService = new FootballService();
 
-
+    function drawMyTeam(players){
+        var template = "";
+        for (let i = 0; i < players.length; i++) {
+            var player = players[i];
+            template += `
+            <div class="player-card">
+                <img src="${player.photo}">
+                <h3>${player.fullname}</h3>
+                <h3>${player.position}</h3>
+                <h3>${player.pro_team}</h3>
+            </div>
+            <button>Add to Team</button>
+            `
+            
+        }
+        document.getElementById("player-info").innerHTML = template;
+    }
 
 
 

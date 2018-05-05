115da8faccd9fa7de488ee2251afb111caca74b1
Madelline Butcher <madellinebutcher@gmail.com>
2018-05-04 23:27:58 -0600

    css updates

--- app/footballController.js
+++ app/footballController.js
@@ -1,30 +1,102 @@
-function FootballController(){
-//PRIVATE
-var footballService = new FootballService();
+function FootballController() {
+    //PRIVATE
+    var footballService = new FootballService();
 
-    function drawMyTeam(players){
-        var template = "";
+
+    function drawMyTeam(players) {
+
+        var template = "<h1>My Team</h1>";
         for (let i = 0; i < players.length; i++) {
             var player = players[i];
             template += `
             <div class="player-card">
-                <img src="${player.photo}">
-                <h3>${player.fullname}</h3>
-                <h3>${player.position}</h3>
-                <h3>${player.pro_team}</h3>
+            <img class="playerImg" src="${player.photo}">
+            <h3>${player.fullname}</h3>
+            <h3>${player.position}</h3>
+            <h3>${player.pro_team}</h3>
             </div>
-            <button>Add to Team</button>
+            <button onclick="app.controllers.footballController.removeFromTeam(${player.id})">Trade</button>
             `
+            if (i > 49) {
+                i = players.length
+            }
+
+        }
+        document.getElementById("myTeam").innerHTML = template;
+    }
+
+
+    function drawPlayers(players) {
+
+        var template = "<h1>The Players</h1>";
+        for (let i = 0; i < players.length; i++) {
+            var player = players[i];
+            template += `
+            <div class="card-group">
+                <div class="card">
+                    <img class="card-img-top" src="${player.photo}"
+                    <div class="card-body">
+                        <h3>${player.fullname}</h3>
+                        <h3>${player.position}</h3>
+                        <h3>${player.pro_team}</h3>
+                    </div>
+                </div>
+            <div class="card-footer">
+            <button class="btn btn-outline-secondary" onclick="app.controllers.footballController.addToTeam(${player.id})">Add to team</button>
+            </div>
+            </div>
             
+            `
+    
+            if (i > 49) {
+                i = players.length
+            }
+
         }
         document.getElementById("player-info").innerHTML = template;
     }
 
 
+    //PUBLIC
 
 
+    this.addToTeam = function addToTeam(id) {
+       footballService.addMyTeam(id, drawMyTeam);
+      };
 
-//PUBLIC
+
+    this.removeFromTeam = function removeFromTeam(id) {
+        footballService.removeFromTeam(id, drawMyTeam)
+      };  
+
+    //OLD SEARCH  
+    // this.searchByTeam = function searchByTeam(e){
+    //     e.preventDefault();
+    //     var teamName = e.target.teamName.value;
+    //     var filteredTeamNames = footballService.getPlayersByTeam(teamName);
+    //     drawPlayers(filteredTeamNames)
+   
+    // };
+    // this.searchByPosition = function searchByPosition(e){
+    //     e.preventDefault();
+    //     var positions = e.target.positions.value;
+    //     var filteredPositions = footballService.getPlayersByPosition(positions);
+    //     drawPlayers(filteredPositions)
+    // };
+    // this.searchPlayersByName = function searchPlayersByName(e){
+    //     e.preventDefault();
+    //     var playerName = e.target.playerName.value;
+    //     var filteredPlayerName = footballService.getPlayersByName(playerName);
+    //     drawPlayers(filteredPlayerName)
+   
+    // };
+
+    this.search = function search(e){
+        e.preventDefault();
+        var query = e.target.query.value
+        var results = footballService.search(query)
+        drawPlayers(results)
+    }
 
 
 
--- app/footballService.js
+++ app/footballService.js
@@ -1,52 +1,107 @@
-function FootballService(callback){
-//PRIVATE
+function FootballService() {
+    //PRIVATE
+
     var playersData = [];
     var myTeam = [];
 
-    function loadPlayersData(){
+  
+    function loadPlayersData() {
         //check if the player already has a copy of the NFL playersData
         var localData = localStorage.getItem('playersData');
-       //if they do, pull from there
-        if(localData){
+        //if they do, pull from there
+        if (localData) {
             playersData = JSON.parse(localData);
             console.log(playersData)
             //return will short-circuit the loadPlayersData function
             //this will prevent the code below from ever executing
-            return callback()
+            return
         }
         //if not go get that data
         var url = "https://bcw-getter.herokuapp.com/?url=";
         var endpointUri = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
         var apiUrl = url + encodeURIComponent(endpointUri);
-  
-          $.getJSON(apiUrl, function(data){
+
+        $.getJSON(apiUrl, function (data) {
             playersData = data.body.players;
             console.log('Player Data Ready')
             console.log('Writing Player Data to localStorage')
             localStorage.setItem('playersData', JSON.stringify(playersData))
             console.log('Finished Writing Player Data to localStorage')
-            callback()
-          });
-      }    
-  loadPlayersData(); //call the function above every time we create a new service
+
+        });
+
+    }
+    loadPlayersData(); //call the function above every time we create a new service
 
 
 
-//PUBLIC
+    //PUBLIC
 
-this.getPlayersByTeam = function getPlayersByTeam(teamName){
 
-};
+    //OLD SEARCH
+    // this.getPlayersByTeam = function getPlayersByTeam(teamName) {
+    //     return playersData.filter(function (player) {
+    //         if (player.pro_team == teamName) {
+    //             return true;
+    //         }
+    //     }
 
-this.getPlayersByPosition = function getPlayersByPosition(position){
+    //     )
+    // };
 
+    // this.getPlayersByPosition = function getPlayersByPosition(positions) {
+    //     return playersData.filter(function (player) {
+    //         if (player.position == positions) {
+    //             return true;
+    //         }
+    //     }
+
+    //     )
+
+    // };
+
+    // this.getPlayersByName = function getPlayersByName(playerName) {
+    //     return playersData.filter(function (player) {
+    //         if (player.fullname == playerName) {
+    //             return true;
+    //         }
+    //     }
+
+    //     )
+    // };
+this.search = function search(query){
+    var searches = query.toLowerCase()
+    var filteredSearches = playersData.filter(function (player){
+        return player.fullname.toLowerCase().includes(searches) || player.pro_team.toLowerCase().includes(searches) || player.position.toLowerCase().includes(searches)
+    })
+    return filteredSearches
 
 }
 
+    this.addMyTeam = function addMyTeam(newPlayerId, cb) {
+       
+        var newPlayer = playersData.find(function (player) {
+            return player.id == newPlayerId
+        })
+        myTeam.push(newPlayer)
+        cb(myTeam);
+    };
+
+    this.removeFromTeam = function removeFromTeam(removeId, draw) {
+        
+        var removeMember = playersData.find(function (player) {
+            return player.id == removeId
+        })
+
+        //indexOf itterates over an array to find the element it was passed and returns the index, if it doesnt find it it will return -1
+        var index = myTeam.indexOf(removeMember)
+        //splice removes object from array
+        myTeam.splice(index, 1)
+
+        draw(myTeam)
 
 
 
 
-
+    }
 }
-
